4c18625ce05511f9dcd52a1b6ff9685889320b32
hectormm1234 <hector.mell24@gmail.com>
2024-01-22 15:26:33 +0100

    Creation of the main
    First function that convert each letter of the text entered by the user to an ASCII art.

---
+++ main.py
@@ -0,0 +1,48 @@
+# We first create a dictionnary to store each letter in ASCII art
+
+ascii_art_dict = {
+    'A': ['  A  ', ' A A ', 'AAAAA', 'A   A', 'A   A'],
+    'B': ['BBBB ', 'B   B', 'BBBB ', 'B   B', 'BBBB '],
+    'C': [' CCC ', 'C   C', 'C    ', 'C   C', ' CCC '],
+    'D': ['DDDD ', 'D   D', 'D   D', 'D   D', 'DDDD '],
+    'E': ['EEEEE', 'E    ', 'EEE  ', 'E    ', 'EEEEE'],
+    'F': ['FFFFF', 'F    ', 'FFF  ', 'F    ', 'F    '],
+    'G': [' GGG ', 'G    ', 'G  GG', 'G   G', ' GGG '],
+    'H': ['H   H', 'H   H', 'HHHHH', 'H   H', 'H   H'],
+    'I': [' III ', '  I  ', '  I  ', '  I  ', ' III '],
+    'J': ['  JJJ', '   J ', '   J ', 'J  J ', ' JJ  '],
+    'K': ['K   K', 'K  K ', 'KK   ', 'K  K ', 'K   K'],
+    'L': ['L    ', 'L    ', 'L    ', 'L    ', 'LLLLL'],
+    'M': ['M   M', 'MM MM', 'M M M', 'M   M', 'M   M'],
+    'N': ['N   N', 'NN  N', 'N N N', 'N  NN', 'N   N'],
+    'O': [' OOO ', 'O   O', 'O   O', 'O   O', ' OOO '],
+    'P': ['PPPP ', 'P   P', 'PPPP ', 'P    ', 'P    '],
+    'Q': [' QQQ ', 'Q   Q', 'Q   Q', 'Q  Q ', ' QQ Q'],
+    'R': ['RRRR ', 'R   R', 'RRRR ', 'R R  ', 'R  RR'],
+    'S': [' SSS ', 'S    ', ' SSS ', '    S', 'SSSS '],
+    'T': ['TTTTT', '  T  ', '  T  ', '  T  ', '  T  '],
+    'U': ['U   U', 'U   U', 'U   U', 'U   U', ' UUU '],
+    'V': ['V   V', 'V   V', 'V   V', ' V V ', '  V  '],
+    'W': ['W   W', 'W   W', 'W W W', 'WW WW', 'W   W'],
+    'X': ['X   X', ' X X ', '  X  ', ' X X ', 'X   X'],
+    'Y': ['Y   Y', ' Y Y ', '  Y  ', '  Y  ', '  Y  '],
+    'Z': ['ZZZZZ', '   Z ', '  Z  ', ' Z   ', 'ZZZZZ']
+}
+
+# Then we can define a function that store the art for a text entered by the user
+
+def text_to_art(text):
+    art_text=[]
+    for letter in text:
+        if letter in ascii_art_dict.keys():
+            for index, line in enumerate(ascii_art_dict[letter]):
+                art_text.append(line)
+    return art_text
+
+# We create a function that will display the art in the console
+def from_art_to_console(art_text):
+    pass
+text='TEST'
+art=[' ' for _ in range(len(text))]*5
+
+print(text_to_art(text))
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
+setup(
+ name='{TD1-Decentralization-Technologies}',
+ version='1.0',
+ author='{hectormm1234}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
